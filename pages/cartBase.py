7a1e9e8b2ea67dc5f89afa6f05fd26a033daa519
Ana Beatriz Araújo de Sousa <ana.araujo.de.sousa@avanade.com>
2025-01-12 22:10:01 -0300

    page objects para test_ct005

--- pages/cartBase.py
+++ pages/cartBase.py
@@ -5,6 +5,7 @@ class CartPage:
     title_product_cart = (By.CSS_SELECTOR, ".cart_item_label "
                                            ".inventory_item_name ")
     remove_button = (By.XPATH, "//button[text() = 'Remove']")
+    button_checkout = (By.ID, "checkout")
 
     def __init__(self, driver):
         self.driver = driver
@@ -16,3 +17,6 @@ class CartPage:
     def remove_one_product(self):
         remove_button = self.driver.find_elements(*self.remove_button)
         remove_button[0].click()
+
+    def click_checkout(self):
+        self.driver.find_element(*self.button_checkout).click()
